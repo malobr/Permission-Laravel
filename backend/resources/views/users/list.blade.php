766dc37afa75ab149141c1fcbc66120ef2e25008
malobr <marcelotc1202@gmail.com>
2025-02-11 17:42:29 -0300

    Video 10 working... 9:45 min...

---
+++ backend/resources/views/users/list.blade.php
@@ -0,0 +1,84 @@
+<x-app-layout>
+    <x-slot name="header">
+        <div class="flex justify-between">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Users') }}
+        </h2>
+        </div>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+
+           <x-message></x-message>
+           
+           <table class="w-full">
+                <thead class="bg-gray-300 rounded-lg shadow shadow-gray-500">
+                    <tr class="border-b">
+                        <th class="px-6 py-3 text-left" width="60">#</th>
+                        <th class="px-6 py-3 text-left">Name</th>
+                        <th class="px-6 py-3 text-left">Email</th>
+                        <th class="px-6 py-3 text-left" width="180">Created</th>
+                        <th class="px-6 py-3 text-center" width="180">Action</th>
+                    </tr>
+                </thead>
+                <tbody class="bg-white">
+                @if ($users->isNotEmpty())
+                @foreach ($users as $user)
+                    
+                    <tr class="border-b">
+                        <td class="px-6 py-3 text-left">
+                            {{$user->id}}
+                        </td>
+                        <td class="px-6 py-3 text-left">
+                            {{$user->name}}
+                        </td>
+                        <td class="px-6 py-3 text-left">
+                            {{$user->email}}
+                        </td>
+                        <td class="px-6 py-3 text-left">
+                            {{\Carbon\Carbon::parse($user->created_at)->format('d M, Y')}}
+                        </td>
+                        <td class="px-6 py-3 text-center">
+                        <a href="{{ route('users.edit', $user->id) }}" class="bg-slate-700 text-sm rounded-md text-white px-3 py-2 hover:bg-slate-600">Edit</a>
+                        <a href="javascript:void(0);" onclick="deleteRole({{ $user->id }})" class="bg-red-700 text-sm rounded-md text-white px-3 py-2 hover:bg-red-600">Delete</a>
+                        </td>
+
+                    </tr>
+                    @endforeach
+                    
+                    @endif
+                </tbody>
+            </table>
+
+            <div class="my-3">
+                {{$users->links()}}
+            </div>
+        </div>
+    </div>
+    <x-slot name="script">
+    <script type="text/javascript">
+        function deleteRole(id) {
+            if(confirm("Are you sure you want to delete this permission?")) {
+                $.ajax({
+                    url: '{{ route("roles.destroy", ":id") }}'.replace(':id', id),
+                    type: "POST",
+                    data: {
+                        id: id,
+                        _method: 'DELETE',
+                        _token: '{{ csrf_token() }}'
+                    },
+                    dataType: "JSON",
+                    success: function(response) {
+                        window.location.href = '{{ route("roles.index") }}';
+                    },
+                    error: function(xhr) {
+                        alert("Error deleting permission");
+                    }
+                });
+            }
+        }
+    </script>
+</x-slot>
+
+</x-app-layout>
